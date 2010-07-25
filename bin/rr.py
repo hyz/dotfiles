41ec76bad9f999ed1e990a87a17670233f8cf458
hyz <jiyong.wu@gmail.com>
2010-07-25 15:18:52 +0800

    binary data describable

---
+++ bin/rr.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import re, struct
+
+data='ffff00022zZoOBB1vVAAMMMM2uuuuUUUUJJJJ'
+desc='{4}4[1[{1}{1}]{2}]{4}1[{4}]{4}'
+
+def pf(lv, desc, n, data):
+    # print ('\t'*lv, '%s loop=%d' % (desc, n), sep=', ')
+    if desc:
+        for x in range(n):
+            current, left = desc, None
+            while 1:
+                current, dx, left = re.match('(.*?)(\d\[|\])(.*)', current).groups()
+                # print ('\t'*lv, current, dx, left, sep=', ')
+                if current:
+                    tmp = current.replace('{', '(.{')
+                    tmp = tmp.replace('}', '})')
+                    # print('\t'*lv, 'USE/CONSUME', current, tmp, sep=', ')
+                    v = re.match('^'+tmp+'(.*)', data).groups()
+                    print('\t'*lv, v)
+                    data = v[-1]
+                if dx == ']' or not left:
+                    break
+                else:
+                    n = int(dx[0])
+                    tmp, data = data[:n], data[n:]
+                    current, data = pf(lv+1, left, int(tmp), data)
+        return left, data
+    return None,0
+
+if __name__ == '__main__':
+    print ('\t'*0 + data)
+    print ('\t'*0 + desc)
+    pf(0, desc+']', 1, data)
+
