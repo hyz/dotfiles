a58a5190375d9fa64a52671eca9e0f4463ff57a6
hyz <jiyong.wu@gmail.com>
2013-12-17 11:01:11 +0800

    magick

---
+++ snip/magick-sample.cpp
@@ -0,0 +1,27 @@
+#include <Magick++.h> 
+#include <string> 
+#include <iostream> 
+
+using namespace std; 
+using namespace Magick; 
+
+int main(int argc, char const *argv[])
+{
+    InitializeMagick(*argv);
+
+    Magick::Image img(argv[1]); 
+    std::cout << img.magick() << "\n"; 
+
+    Magick::Geometry new_size("230x230");
+    Magick::Geometry old_size = img.size();
+    if(old_size.height()>new_size.height() || old_size.width()>new_size.width())
+    {   
+        img.sample(new_size);
+        std::cout << "230x230 \n";
+    }
+
+    img.write(std::string("out.") + img.magick());
+
+    return 0;
+}
+
