f045fb2b87b8070f9c4772bfe19f2c60bfe8d274
hyz <jiyong.wu@gmail.com>
2014-01-25 19:08:54 +0800

    u

--- snip/c++/size-asio-iostream.cpp
+++ snip/c++/size-asio-iostream.cpp
@@ -1,11 +1,13 @@
-#include <boost/asio.hpp>
+//#include <boost/asio.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << sizeof(boost::asio::streambuf) << std::endl;
+    // std::cout << sizeof(boost::asio::streambuf) << std::endl;
     std::cout << sizeof(std::streambuf) << std::endl;
     std::cout << sizeof(std::ostream) << std::endl;
+
+    return 0;
 }
 
 
