bd147a7f6d28aec03cb86b1feb1101401df74938
hyz <jiyong.wu@gmail.com>
2014-04-20 20:17:37 +0800

    u

--- snip/cap.sh
+++ snip/cap.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
-# Usage: sh cap.sh pcap/em1/9000 -i em1 port 9000
+#
+# @hourly cd /home/wood && sh bin/cap.sh pcap/em1/9900 -s0 -i em1 port 9900
+# $ tcpdump -nn -SA -r pcap/em1/9900 host ... port ..
 
-Dir=$1 ; shift
+Tag=$1 ; shift
 
-[ -z "$Dir" ] && exit 1
-[ -d "$Dir" ] || mkdir -p $Dir
+[ -z "$Tag" ] && exit 2
+[ -d "$Tag" ] || mkdir -p $Tag
 
-if [ -r "$Dir/pid" ] ; then
-    pid=`cat $Dir/pid`
-    kill $pid && sleep 1 && kill -9 $pid
+exec >> /tmp/`basename $0`.output
+exec 2>&1
+echo $$
+pwd
+
+if [ -r "$Tag/pid" ] ; then
+    pid=`cat $Tag/pid`
+    kill $pid
+    sh -c "sleep 1 ; kill -9 $pid" &
 fi
 
-echo $$ > $Dir/pid
-exec tcpdump -nn -s0 -w "$Dir/`date +%H`.cap" $*
+echo $$ > $Tag/pid
+exec /usr/sbin/tcpdump -nn -w "$Tag/`date +%H`" $*
 
