155316cba8b053269d993d7d66e4e630ebce4b8f
hyz <jiyong.wu@gmail.com>
2016-05-26 09:54:25 +0800

    u

---
+++ workspace/samples/spirit-utf8.cc
@@ -0,0 +1,27 @@
+#include <boost/regex/pending/unicode_iterator.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/range.hpp>
+#include <iterator>
+#include <iostream>
+#include <ostream>
+#include <cstdint>
+#include <vector>
+
+int main()
+{
+    using namespace boost;
+    using namespace spirit::qi;
+    using namespace std;
+
+    auto &&utf8_text=u8"你好，世界！";
+    u8_to_u32_iterator<const char*>
+        tbegin(begin(utf8_text)), tend(end(utf8_text));
+
+    vector<uint32_t> result;
+    parse(tbegin, tend, *standard_wide::char_, result);
+    for(auto &&code_point : result)
+        cout << "&#" << code_point << ";";
+    cout << endl;
+}
+
+// http://stackoverflow.com/questions/13679669/how-to-use-boostspirit-to-parse-utf-8?rq=1
