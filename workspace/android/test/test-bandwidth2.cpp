8132ea8a8f53f116b18f840b9cc231f8fea7f0ce
hyz <jiyong.wu@gmail.com>
2016-06-03 13:49:23 +0800

    u

--- workspace/android/test/test-bandwidth2.cpp
+++ workspace/android/test/test-bandwidth2.cpp
@@ -16,43 +16,115 @@
 #include <boost/noncopyable.hpp>
 
 inline void exit127_(int,int) { exit(127); }
-#define ERR_EXIT(...) exit127_( fprintf(stderr, __VA_ARGS__), fprintf(stderr, " error:%d: %s\n",__LINE__, strerror(errno)) )
-#define ERR_MSG(...) (void)( fprintf(stderr, __VA_ARGS__), fprintf(stderr, " error:%d: %s\n",__LINE__, strerror(errno)) )
+#define ERR_EXIT(...) exit127_( fprintf(stderr, __VA_ARGS__), fprintf(stderr, ": %s error:%d\n", strerror(errno),__LINE__) )
+#define ERR_MSG(...) (void)( fprintf(stderr, __VA_ARGS__), fprintf(stderr, ": %s error:%d\n", strerror(errno),__LINE__) )
 #define DBG_MSG(...) (void)( fprintf(stderr, __VA_ARGS__), fprintf(stderr, " debug:%d\n",__LINE__) )
 
-typedef struct sockaddr SA;
+static unsigned sMills(struct timeval const& tv0) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (1000*1000*(tv.tv_sec - tv0.tv_sec) + tv.tv_usec - tv0.tv_usec)/1000;
+}
 
+typedef struct sockaddr SA;
+static const socklen_t SOCKLEN_ = sizeof(struct sockaddr_in);
+
+static void sockaddr_init(struct sockaddr_in* sa, char const* ip, short port)
+{
+    bzero(sa, sizeof(struct sockaddr_in));
+    sa->sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &sa->sin_addr);
+    sa->sin_port = htons( port );
+}
+static struct sockaddr_in sockaddr_init(char const* ip, short port)
+{
+    struct sockaddr_in sa;
+    sockaddr_init(&sa, ip, port);
+    return sa;
+}
+
+struct TCPAcceptor {
+    int socket_;
+    TCPAcceptor(char const* bind_ip, short bind_port) {
+        socket_ = socket(AF_INET, SOCK_STREAM, 0);
+        struct sockaddr_in sa;
+        sockaddr_init(&sa, bind_ip, bind_port);
+        if (bind(socket_, (SA*)&sa, SOCKLEN_) < 0)
+            ERR_EXIT("bind %s:%d", bind_ip, bind_port);
+        if (listen(socket_, 96) < 0)
+            ERR_EXIT("listen %s:%d", bind_ip, bind_port);
+    }
+    ~TCPAcceptor() { close(socket_); }
+
+    int accept(struct sockaddr_in *sa) {
+        socklen_t slen = sizeof(struct sockaddr_in);
+        return ::accept(socket_, (SA*)sa, &slen);
+    }
+    int accept() {
+        struct sockaddr_in sa;
+        return this->accept(&sa);
+    }
+};
+int tcp_connect(char const* dst_ip, short dst_port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in sa;
+    sockaddr_init(&sa, dst_ip, dst_port);
+    if (connect(sockfd, (SA*)&sa, SOCKLEN_) < 0)
+        ERR_EXIT("connect %s:%d", dst_ip, dst_port);
+    return sockfd;
+}
+int udp_bind(char const* ip, short port)
+{
+    struct sockaddr_in sa;
+    sockaddr_init(&sa,ip, port);
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (bind(sockfd, (SA*)&sa, SOCKLEN_) < 0) {
+        ERR_EXIT("bind %s:%d", ip, port);
+    }
+    return sockfd;
+}
+int udp_connect(char const* ip, short port)
+{
+    struct sockaddr_in saddr;
+    socklen_t slen = sizeof(struct sockaddr_in);
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    sockaddr_init(&saddr,ip, port);
+    if (connect(sockfd, (SA*)&saddr, slen) < 0) {
+        ERR_EXIT("connect %s:%d", ip, port);
+    }
+    return sockfd;
+}
+
+template <typename Proto>
 struct Main : boost::noncopyable
 {
-    Main(char const* dst_ip, int dst_port, int Nps, int Kps)
+    Proto proto_;
+    Main(int sfd, int Nps, int Kps) //: io_(tcp, dst_ip, dst_port)
     {
-        singleton = this;
-        gettimeofday(&tv0_, NULL);
-        bzero((void*)&sent_,sizeof(sent_));
-        bzero((void*)&recv_,sizeof(recv_));
+        socket_ = sfd;
 
-        numbps_ = numbpx_ = Nps;
-        numbrush_ = numbrx_ = 0;
-        bytes1_ = std::max(Kps*1024/Nps, 1500);
+        bzero(&sent_, sizeof(sent_));
+        bzero(&recv_, sizeof(recv_));
 
-        sockaddr_init(&saddr_, dst_ip, dst_port); // socket_ = udp_connect(dst_ip, dst_port);
-        socket_ = socket(AF_INET, SOCK_DGRAM, 0);
+        wmills_ = 1000/(Nps);
+        bytes1_ = std::min( std::max(Kps*1024/Nps, 1024*4), 1024*56 );
+        npf_ = nif_ = 0;
+
         runc_ = 0x7fffffff;
         run_ = &Main::do_send;
-        printf("sending ...\n");
     }
-    Main(short bind_port)
+    Main(int sfd)
     {
-        singleton = this;
-        gettimeofday(&tv0_, NULL);
-        bzero((void*)&sent_,sizeof(sent_));
-        bzero((void*)&recv_,sizeof(recv_));
-        socket_ = udp_bind("0", bind_port);
+        socket_ = sfd;
+
+        bzero(&sent_, sizeof(sent_));
+        bzero(&recv_, sizeof(recv_));
+
         runc_ = 0x7fffffff;
         run_ = &Main::do_receive;
-        printf("receiving ...\n");
     }
-    ~Main() { close(socket_); }
+    ~Main() { ::close(socket_); }
 
     void run() {
         srand(time(0));
@@ -62,15 +134,10 @@ struct Main : boost::noncopyable
     }
 
 private:
-    struct timeval tv0_;
-    static unsigned sMills() {
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-        return (1000*1000*(tv.tv_sec - singleton->tv0_.tv_sec) + tv.tv_usec - singleton->tv0_.tv_usec)/1000;
-    }
-
     struct recv_st
     {
+        struct timeval tv0_;
+
         unsigned seq;
         unsigned n_dgram;
         unsigned bytes_recvd;
@@ -78,7 +145,7 @@ private:
 
         void incoming(unsigned len, unsigned seq, unsigned nbytes) {
 			if (n_dgram == 0)
-				gettimeofday(&Main::singleton->tv0_, NULL);
+				gettimeofday(&tv0_, NULL);
             recv_st prev = *this;
 
             this->n_dgram++;
@@ -96,7 +163,7 @@ private:
                 seqv = legv[(seq<prev.seq ? -1 : (seq==prev.seq ? 0 : 1)) +1];
             }
 
-            unsigned ct = Main::sMills();
+            unsigned ct = sMills(tv0_);
             if (seqv || ((ct - tpr_) > 5 && (seq&0x7f)==0) || (seq&0x7ff) == 0 || seq<3) {
                 int nsec = (ct)/1000;
                 if (nsec == 0)
@@ -113,10 +180,14 @@ private:
     } recv_;
 
     struct sent_st {
+        struct timeval tv0_;
         unsigned seq;
         unsigned bytes_sent;
 
         void outgoing(unsigned, unsigned bytes) {
+            if (seq == 0) {
+				gettimeofday(&tv0_, NULL);
+            }
             sent_st const& prev = *this;
             seq++;
             bytes_sent += bytes;
@@ -126,7 +197,7 @@ private:
         //unsigned bytes_psec_ = 1024*300;
 
         void print(sent_st const&) const {
-            unsigned ct = Main::sMills();
+            unsigned ct = sMills(tv0_);
             int nsec = ct/1000;
             if (nsec == 0)
                 nsec = 1;
@@ -140,12 +211,9 @@ private:
     } sent_;
 
 private:
-    unsigned char numbps_, numbpx_, numbrush_, numbrx_;
+    short npf_, nif_;
+    short wmills_;
     unsigned short bytes1_;
-
-    unsigned runc_; // = 0x7fffffff;
-    void (Main::*run_)();
-    static Main* singleton;
 
     int socket_;
     enum { max_length = 1024*80 };
@@ -153,8 +221,9 @@ private:
 
     void do_receive()
     {
-        int len = recv(socket_, (void*)data_, sizeof(data_), 0);
-        if (len < 32) {
+        int len = proto_.recv(socket_, data_, sizeof(data_));
+        //int len = ::read(socket_, (void*)data_, sizeof(data_));
+        if (len < 20) {
             ERR_EXIT("recv %d", len);
         }
         recv_.incoming(ntohl(data_[0]), ntohl(data_[1]), len);
@@ -162,19 +231,22 @@ private:
 
     void do_send()
     {
-        if (numbrx_ >= numbrush_ && numbpx_ >= numbps_) {
-            numbrx_ = numbpx_ = 0;
-            numbrush_ = 3+rand()%3;
+        if (nif_ <= 0 && npf_ <= 0) {
+            nif_ = 3+rand()%3;
+            npf_ = 1000/wmills_ * (100 + rand() % 200) / 100;
+            DBG_MSG("IP/frame %d %d", nif_, npf_);
         }
 
-        int wmills = 1000/(numbps_+1);//int raval = rand()%100;
-        unsigned length = unsigned(bytes1_) + rand()%3000 - 1500; //= 1024*( (raval>90?15:5) + raval%5);
-        if (numbrx_ < numbrush_) {
-            numbrx_++;
+        int wmills = this->wmills_;
+        unsigned length = bytes1_;
+
+        if (nif_ > 0) {
+            if (--nif_ == 0)
+                length += (rand()%1000 - 1500);
             wmills = 0;
-            //length = length * (1 + rand()%200/100.0);
-        } else if (numbpx_ < numbps_) {
-            numbpx_++;
+        } else if (npf_ > 0) {
+            npf_--;
+            length += (rand()%3000 - 1500);
         }
         if (length > 1024*56) {
             DBG_MSG("%u > 56K", length/1024);
@@ -185,8 +257,9 @@ private:
         data_[1] = htonl(sent_.seq);
         //data_[2] = htonl(1);
 
-        // fd_set fds; //FD_ZERO(&fds); //FD_SET(socket_, &fds);
-        int retval = sendto(socket_, (void*)data_, length, 0, (SA*)&saddr_, (socklen_t)sizeof(saddr_));
+        int retval = proto_.send(socket_, data_, length);
+        //fd_set fds; //FD_ZERO(&fds); //FD_SET(socket_, &fds);
+        //int retval = ::write(socket_, (void*)data_, length);
         if (retval < 0) 
             ERR_EXIT("sendto");
         sent_.outgoing(sent_.seq, length);
@@ -194,60 +267,69 @@ private:
         if (wmills > 0) {
             struct timeval tv;
             tv.tv_sec = 0;
-            tv.tv_usec = 1000 * wmills;//(numbex_>0 ? 1 : 1000/numbps_)*1000;
+            tv.tv_usec = 1000 * wmills;
             select(0, NULL, NULL, NULL, &tv);
             //std::this_thread::sleep_for(std::chrono::milliseconds(mils_idle_));
         }
     }
 
 private:
-    struct sockaddr_in saddr_;
+    unsigned runc_; // = 0x7fffffff;
+    void (Main::*run_)();
+};
 
-    static void sockaddr_init(struct sockaddr_in* sa, char const* ip, short port)
-    {
-        bzero((char *) sa, sizeof(struct sockaddr_in));
-        sa->sin_family = AF_INET;
-        inet_pton(AF_INET, ip, &sa->sin_addr);
-        sa->sin_port = htons( port );
+struct ProtocolTCP {
+    int recv(int sfd, int32_t* buf, unsigned len) {
+        int nread = ::read(sfd, &buf[0], 20);
+        if (nread != 20)
+            ERR_EXIT("read %d", nread);
+        int lenv = ntohl(buf[0]);
+        if (lenv > int(len) || lenv <= 20)
+            ERR_EXIT("[0]=%d %u", lenv, len);
+        nread = ::read(sfd, &buf[5], lenv-20);
+        return ( (nread < 1) ? nread : 20+nread);
     }
-    static int udp_connect(char const* ip, short port)
-    {
-        struct sockaddr_in saddr;
-        socklen_t slen = sizeof(struct sockaddr_in);
-        int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-        sockaddr_init(&saddr,ip, port);
-        if (connect(sockfd, (SA*)&saddr, slen) < 0) {
-            ERR_EXIT("connect %s:%d", ip, port);
-        }
-        return sockfd;
-    }
-    static int udp_bind(char const* ip, short port)
-    {
-        struct sockaddr_in saddr;
-        socklen_t slen = sizeof(struct sockaddr_in);
-        int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-        sockaddr_init(&saddr,ip, port);
-        if (bind(sockfd, (SA*)&saddr, slen) < 0) {
-            ERR_EXIT("bind %s:%d", ip, port);
-        }
-        return sockfd;
+    int send(int sfd, int32_t* buf, unsigned len) {
+        return ::write(sfd, &buf[0], len);
     }
 };
-Main* Main::singleton;
+struct ProtocolUDP {
+    int recv(int sfd, int32_t* buf, unsigned len) {
+        return ::recv(sfd, buf, len, 0);
+    }
+    int send(int sfd, int32_t* buf, unsigned len) {
+        return ::send(sfd, buf, len, 0);
+    }
+};
 
 int main(int argc, char* argv[])
 {
     try {
         if (argc == 5) {
-            Main sender(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
-            sender.run();
+            printf("%s sending ...\n", argv[0]);
+            if (strstr(argv[0], "tcp")) {
+                Main<ProtocolTCP> sender( tcp_connect(argv[1],atoi(argv[2]))
+                        , atoi(argv[3]), atoi(argv[4]));
+                sender.run();
+            } else {
+                Main<ProtocolUDP> sender(udp_connect(argv[1],atoi(argv[2]))
+                        , atoi(argv[3]), atoi(argv[4]));
+                sender.run();
+            }
         } else if (argc == 2) {
-            Main receiver( atoi(argv[1]) );
-            receiver.run();
+            printf("%s receiving ...\n", argv[0]);
+            if (strstr(argv[0], "tcp")) {
+                TCPAcceptor a("0", atoi(argv[1]));
+                Main<ProtocolTCP> receiver( a.accept() );
+                receiver.run();
+            } else {
+                Main<ProtocolUDP> receiver( udp_bind("0", atoi(argv[1])) );
+                receiver.run();
+            }
         } else {
-            fprintf(stderr,"Usage: a.out [host] <port> [idle-mills]\n");
+            fprintf(stderr,"Usage: \n"
+                    "  send:\ta.out <host> <port> <Npkg/s> <KB/s>\n"
+                    "  recv:\ta.out <port>\n");
             exit(1);
         }
     } catch (std::exception const& e) {
