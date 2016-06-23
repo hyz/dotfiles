579962da4f626d967ec25e874b1a25d044399f67
hyz <jiyong.wu@gmail.com>
2016-06-23 14:06:51 +0800

    u

---
+++ workspace/android/javah/com/hello/world.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+class world
+{
+    public native void foo();
+    public static native void hello();
+    public static native String about();
+    public static native String about(int index);
+}
+
---
+++ workspace/android/javah/javah.sh
@@ -0,0 +1,3 @@
+
+javah com.hello.world
+
