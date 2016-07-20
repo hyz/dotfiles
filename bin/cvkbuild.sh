82813372f8f35e0f4962396048f58cfe2041c66b
hyz <jiyong.wu@gmail.com>
2016-07-20 15:44:59 +0800

    u

--- bin/cvkbuild.sh
+++ bin/cvkbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-err_exit() {
+die() {
     echo $* ; exit 1 ;
 }
 
@@ -12,18 +12,18 @@ while true ; do
         -V|--version) Newver=$2 ; shift 2 ;;
         --upload) _upload=1 ; shift ;;
         --) shift ; break ;;
-        *) err_exit "options" ; exit 1 ;;
+        *) die "options" ; exit 1 ;;
     esac
 done
 repo=`echo "$1" | sed -e 's/^[./]\+//g' -e 's/[./]\+$//g'`
 builddir=`echo "$2" | sed 's/[./]\+$//g'` ##/mnt/hgfs/home/svnchina/
-cwd=`pwd`
 
-[ -d "$repo" -a -d "$1" ] || err_exit "repo-dir error: $1"
-[ -d "$builddir" -a -d "$2" ] || err_exit "destination-dir error: $2"
+[ -d "$repo" -a -d "$1" ] || die "repo-dir error: $1"
+[ -d "$builddir" -a -d "$2" ] || die "destination-dir error: $2"
 case "$variant"  in
-    test|release) ;;
-    *) err_exit "variant error: $variant" ;;
+    test) ;;
+    release) ;;
+    *) die "variant error: $variant" ;;
 esac
 
 svn st $repo
@@ -31,14 +31,18 @@ svn st $repo
 NewSVNRev=`svn info $repo |grep -Po '^Revision:\s+\K\d+'`
 Apk="Game-newsvn$NewSVNRev-$(date +%Y%m%d).apk"
 
-if [ x"$_upload" = x1 ] ; then
-    # echo "cd \"$builddir\" && rsync -vr build-apk $cwd" |sh -
-    reldir=build-release/$(date +%F)
-    mkdir -p $reldir
-    cp -v $builddir/$repo/libmtkhw.so $builddir/$repo/libs/armeabi-v7a/libBarcode.so $reldir/
-    cp -v $builddir/build-apk/$Apk $reldir/Game.apk
+if [ -n "$_upload" ] ; then
+    host_ip=192.168.2.113
+    [ "$variant" = release ] || die "release required"
+    #cwd=`pwd`
+    reldir=build/$variant/$(date +%m%d%H%M).$NewSVNRev
+    [ -d "$reldir" ] || mkdir -p $reldir || die "$reldir"
+    cp -v $builddir/$repo/libmtkhw.so $reldir/ || die "libmtkhw.so"
+    cp -v $builddir/$repo/libs/armeabi-v7a/libBarcode.so $reldir/ || die "libBarcode.so"
+    cp -v $builddir/$variant/$Apk $reldir/Game.apk || die "$Apk"
     find $reldir -type f -exec chmod a-x '{}' \;
-    rsync -vr $reldir 192.168.2.113:build-release/
+    rsync -vrR $reldir $host_ip:. || die "$host_ip"
+    echo "Copyed: $host_ip"
     exit
 fi
 
@@ -50,7 +54,7 @@ Oldver=`tr -d ' \t' <$AppConfig |grep -Po '^publicstaticfinalStringVERSION="v\K[
 if [ "$variant" = release ] ; then
     if [ -z "$Newver" ] ; then
         Newver=`echo $Oldver | awk -F. '{print $1"."$2"."$3+1}'`
-        [ -n "$Newver" ] || err_exit "Incr VERSION fail"
+        [ -n "$Newver" ] || die "Incr VERSION fail"
     fi
     sed -i '/^\s*public.\+\<VERSION\s*=/{s/"v[0-9]\+\.[0-9]\+\.[0-9]\+"/"v'$Newver'"/}' $AppConfig
     sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' $AppConfig
@@ -83,15 +87,14 @@ echo
 find $builddir/$repo -name log.h -o -name AppConfig.java -o -name Crypto.bat
 svn st $repo
 
-echo
-echo "variant: $variant"
-echo "Revision: $OldSVNRev => $NewSVNRev"
-echo "Version: $Oldver => $Newver"
 echo "diff -r --brief $repo $builddir/$repo"
 echo "svn diff $AppConfig"
+echo
+echo "Revision: $OldSVNRev => $NewSVNRev"
+echo "Version: $Oldver => $Newver"
+echo "Output required: $variant/$Apk"
 
-echo "$Apk"
 ###
-# ls $builddir/build-apk/Game-newsvn$NewSVNRev-$(date +%Y%m%d).apk
+# ls $builddir/$Apk
 # find $builddir/$repo -name libmtkhw.so -o -name libBarcode.so
 
