ac19842d12c3f22e546ea19f89f727b54a8b9e43
hyz <jiyong.wu@gmail.com>
2016-08-03 18:18:48 +0800

    u

--- bin/hzmake.sh
+++ bin/hzmake.sh
@@ -44,6 +44,9 @@ variant=$2
 #[ `stat -c\%i $builddir` = `stat -c\%i $2` ] || die "destination-dir: $2"
 
 AppConfig=src/com/huazhen/barcode/app/AppConfig.java
+if [ "$what" = prepare -o "$what" = init ] ; then
+    svn revert $repo/$AppConfig
+fi
 NewSVNRev=`svn info $repo |grep -Po '^Revision:\s+\K\d+'`
 Ver=`tr -d ' \t' <$repo/$AppConfig |grep -Po '^publicstaticfinalStringVERSION="v\K[^"]+'`
 
@@ -88,11 +91,12 @@ rbuild)
 
 sync-up)
     rm -rf /tmp/$Rev #$variant/$Rev
-    mkdir /tmp/$Rev/lib
-    mkdir /tmp/$Rev/internal
+    mkdir -p /tmp/$Rev/lib
+    mkdir -p /tmp/$Rev/internal
 
-    cp -v $builddir/$repo/libmtkhw.so /tmp/$Rev/lib/ || die "libmtkhw.so"
-    cp -v $builddir/$repo/libs/armeabi-v7a/libBarcode.so /tmp/$Rev/lib/ || die "libBarcode.so"
+    svname=`basename $repo`
+    cp -v $builddir/$svname/libmtkhw.so /tmp/$Rev/lib/ || die "libmtkhw.so"
+    cp -v $builddir/$svname/libs/armeabi-v7a/libBarcode.so /tmp/$Rev/lib/ || die "libBarcode.so"
     cp -v $builddir/$variant/$Apk /tmp/$Rev/internal/Game.apk || die "$Apk"
 
     find /tmp/$Rev -type f -exec chmod 0644 '{}' \;
@@ -101,8 +105,6 @@ sync-up)
     ;;
 
 prepare|init)
-    # svn up $repo
-    svn revert $repo/$AppConfig
     svn st $repo
     OldSVNRev=`tr -d ' \t' <$repo/$AppConfig |grep -Po '^publicstaticfinalStringSVNVERSION="new-svn\K[^"]+'`
 
@@ -115,39 +117,40 @@ prepare|init)
         sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' $repo/$AppConfig
     fi
 
-    rm -rf $builddir/$repo
-    find $repo \( -name ".svn" -o -name ".git*" \) -prune -o -print \
-        | rsync --no-g --no-o --files-from=- . $builddir
+    svname=`basename $repo`
+    rm -rf $builddir/$svname
+    ( cd $repo/.. && find $svname \( -name ".svn" -o -name ".git*" \) -prune -o -print \
+        | rsync --files-from=- . $builddir )
 
     if [ "$variant" = release ] ; then
-        sed -i '/^[^#]\+#\s*define\s\+BUILD_RELEASE/{s:^[^#]\+::}' $builddir/$repo/jni/Utils/log.h
-        cp -v $repo/../tools/CryptoRelease.bat $builddir/$repo/tools/Crypto.bat
+        sed -i '/^[^#]\+#\s*define\s\+BUILD_RELEASE/{s:^[^#]\+::}' $builddir/$svname/jni/Utils/log.h
+        cp -v $repo/../tools/CryptoRelease.bat $builddir/$svname/tools/Crypto.bat
     else
         sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' \
-            $builddir/$repo/$AppConfig
-        testoutdir=" \\192.168.2.115\\Release1\\test\\"
+            $builddir/$svname/$AppConfig
+        testoutdir=" \\\\192.168.2.115\\Release1\\test\\"
     fi
 
-    echo "diff -r --brief $repo $builddir/$repo"
-    diff -r --brief $repo $builddir/$repo |grep -v '.svn'
+    echo ": diff -r --brief $repo $builddir/$svname"
+    diff -r --brief $repo $builddir/$svname |grep -v '.svn'
     echo
-    diff $repo/$AppConfig $builddir/$repo/$AppConfig
-    diff $repo/jni/Utils/log.h $builddir/$repo/jni/Utils/log.h
-    diff --brief $repo/../tools/CryptoRelease.bat $builddir/$repo/tools/Crypto.bat
+    diff $repo/$AppConfig $builddir/$svname/$AppConfig
+    diff $repo/jni/Utils/log.h $builddir/$svname/jni/Utils/log.h
+    #diff --brief $repo/../tools/CryptoRelease.bat $builddir/$svname/tools/Crypto.bat
 
     #echo
     #find $builddir/$repo -name log.h -o -name AppConfig.java -o -name Crypto.bat
 
     echo "SVN Revision: $OldSVNRev => $NewSVNRev"
     echo "Version: $Ver => $Newver $Vertag"
-    svn log -r$OldSVNRev:$NewSVNRev $repo > /tmp/svn.log.$OldSVNRev-$NewSVNRev
     echo
+    svn log -r$OldSVNRev:$NewSVNRev $repo > svn.log.$OldSVNRev-$NewSVNRev
     echo "svn.log.$OldSVNRev-$NewSVNRev"
     echo "$variant/$Apk$testoutdir"
     # grep word howto.txt
     echo
     svn st $repo
-    echo "svn diff $repo/$AppConfig"
+    #echo "svn diff $repo/$AppConfig"
     #echo "svn commit androidbarcode"
     ;;
 
--- bin/hzrbuild.sh
+++ bin/hzrbuild.sh
@@ -18,9 +18,9 @@ dn=`dirname "$out"`
 [ -d "$dn" ] || mkdir -p "$dn" || die "mkdir: $dn"
 
 if [ "$plat" = "cvk350t" ] ; then
-    reldir1="/osca/release/$plat-iphone-" # `date +%Y-%m-%d-%H-%M`
+    reldir1="/osca/release/$plat-iphone" # `date +%Y-%m-%d-%H-%M`
 else
-    reldir1="/osca/release/$plat-"
+    reldir1="/osca/release/$plat"
 fi
 
 echo `pwd` $builddir $appdir $reldir1 $out
@@ -28,7 +28,7 @@ echo `pwd` $builddir $appdir $reldir1 $out
 ( cd $appdir && find * -type f | /bin/cpio --verbose -pu $builddir/vendor/g368_noain_t300/application ) || die "cpio"
 ( cd $builddir && ./mk -o=TARGET_BUILD_VARIANT=user systemimage && ./autocopy ) || die "mk & autocopy"
 
-/bin/ln -snf "$reldir1`date +%Y-%m-%d-%H-%M`" "$out" 
+/bin/ln -snf "$reldir1-`date +%Y-%m-%d-%H-%M`" "$out" 
 #/bin/mv "/osca/release/$Ot-`date +%Y-%m-%d-%H-%M`" "$plat-$ver-`date +%Y%m%d`"
 echo "$out [OK]"
 
