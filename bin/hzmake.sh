73025357806bc19b793ee68a19270db9ac6dbc2e
hyz <jiyong.wu@gmail.com>
2016-08-05 15:00:50 +0800

    u

--- bin/hzmake.sh
+++ bin/hzmake.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # export REPO=androidbarcode VARIANT=release PLATS=k400,cvk350c,cvk350t
-# hzmake.sh prepare    $REPO $VARIANT -P $PLATS
-# hzmake.sh sync-up    $REPO $VARIANT -P $PLATS
-# hzmake.sh rbuild     $REPO $VARIANT -P $PLATS
-# hzmake.sh sync-down  $REPO $VARIANT -P $PLATS
-# hzmake.sh rar        $REPO $VARIANT -P $PLATS -p XXX
-# hzmake.sh svn-commit $REPO $VARIANT -P $PLATS
+# hzmake.sh prepare   $REPO $VARIANT -P $PLATS
+# hzmake.sh sync-up   $REPO $VARIANT -P $PLATS
+# hzmake.sh rbuild    $REPO $VARIANT -P $PLATS
+# hzmake.sh sync-down $REPO $VARIANT -P $PLATS
+# hzmake.sh rar       $REPO $VARIANT -P $PLATS -p XXX
+# hzmake.sh version-up $REPO $VARIANT -P $PLATS
 
 die() {
     echo $* ; exit 1 ;
@@ -26,7 +26,7 @@ while true ; do
     case "$1" in
         #-v|--variant) variant=$2 ; shift 2 ;;
         -P|--plats) plats=$2 ; shift 2 ;;
-        -V|--ver) Newver=$2 ; shift 2 ;;
+        -V|--ver) NewVer=$2 ; shift 2 ;;
         -t|--vertag) Vertag="$2" ; Vertag1="-$2"; shift 2 ;;
         -p|--rarpwd) rarpwd=$2 ; shift 2 ;;
         -d|--outdir) outdir=$2 ; shift 2 ;;
@@ -43,29 +43,43 @@ variant=$2
 [ -d "$builddir/$variant" ] || mkdir -p $builddir/$variant || die "mkdir $builddir/$variant"
 #[ `stat -c\%i $builddir` = `stat -c\%i $2` ] || die "destination-dir: $2"
 
+prjname=`basename $repo`
 AppConfig=src/com/huazhen/barcode/app/AppConfig.java
-if [ "$what" = prepare -o "$what" = init ] ; then
-    svn revert $repo/$AppConfig
-fi
+#if [ "$what" = prepare -o "$what" = init ] ; then
+#    svn revert $repo/$AppConfig
+#fi
+
+OldVer=`tr -d ' \t' <$repo/$AppConfig |grep -Po '^publicstaticfinalStringVERSION="v\K[^"]+'`
+OldSVNRev=`tr -d ' \t' <$repo/$AppConfig |grep -Po '^publicstaticfinalStringSVNVERSION="new-svn\K[^"]+'`
+NewVer=`echo $OldVer | awk -F. '{print $1"."$2"."$3+1}'`
 NewSVNRev=`svn info $repo |grep -Po '^Revision:\s+\K\d+'`
-Ver=`tr -d ' \t' <$repo/$AppConfig |grep -Po '^publicstaticfinalStringVERSION="v\K[^"]+'`
 
 Apk="Game-newsvn$NewSVNRev-$(date +%Y%m%d)$Vertag1.apk"
-Rev=$Vertag$Ver-$NewSVNRev #-$(date +%m%d) # local temp application dir
+VerF=$Vertag$NewVer-$NewSVNRev #-$(date +%m%d) # local temp application dir
 
 case "$what" in
 clean)
     ;;
 
-svn-commit)
+version-up)
+    [ "$variant" = release ] || die "commit: $variant"
+
+    sed -i '/^\s*public.\+\<VERSION\s*=/{s/"v[0-9]\+\.[0-9]\+\.[0-9]\+"/"v'$NewVer'"/}' $repo/$AppConfig
+    sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' $repo/$AppConfig
     svn diff $repo
-    svn commit $repo -m "Version($Ver) updated"
+    echo "$OldVer => $NewVer, $OldSVNRev => $NewSVNRev, $repo/$AppConfig"
+    echo "commit $repo? (y/n)"
+    read yn
+    case "$yn" in
+        y|Y) svn commit $repo -m "Version($OldVer=>$NewVer, $OldSVNRev=>$NewSVNRev) updated" ;;
+        *) svn revert $repo/$AppConfig ;;
+    esac
     ;;
 
 rar)
     which rar || die "rar not-found"
     for plat in ${plats//,/ } ; do
-        rel="$plat-$Ver-`date +%Y%m%d`"
+        rel="$plat-$NewVer-`date +%Y%m%d`"
         ar="${rel#cvk}.rar"
         rm -f $outdir/$ar
         ( cd $outdir && rar a -hp$rarpwd $ar $rel ) || die "$ar"
@@ -75,83 +89,67 @@ rar)
 
 sync-down)
     for plat in ${plats//,/ } ; do
-        rel="$plat-$Ver-`date +%Y%m%d`"
+        rel="$plat-$NewVer-`date +%Y%m%d`"
         rsync -vrL $rhost:$variant/$rel $outdir/ || die "$rhost $rel"
     done
     ;;
 
 rbuild)
     for plat in ${plats//,/ } ; do
-        rel="$plat-$Ver-`date +%Y%m%d`"
-        ssh root@$rhost "cd $rhome && bin/hzrbuild.sh $variant/application/$Rev $variant/$rel $plat" || die "ssh rbuild"
+        rel="$plat-$NewVer-`date +%Y%m%d`"
+        ssh root@$rhost "cd $rhome && bin/hzrbuild.sh $variant/application/$VerF $variant/$rel $plat" || die "ssh rbuild"
+        echo "$rhost:$rhome/$variant/$rel [OK]"
     done
-
-    echo "$rhost:$rhome/$variant/$rel [OK]"
     ;;
 
 sync-up)
-    rm -rf /tmp/$Rev #$variant/$Rev
-    mkdir -p /tmp/$Rev/lib
-    mkdir -p /tmp/$Rev/internal
+    rm -rf /tmp/$VerF #$variant/$VerF
+    mkdir -p /tmp/$VerF/lib
+    mkdir -p /tmp/$VerF/internal
 
-    svname=`basename $repo`
-    cp -v $builddir/$svname/libmtkhw.so /tmp/$Rev/lib/ || die "libmtkhw.so"
-    cp -v $builddir/$svname/libs/armeabi-v7a/libBarcode.so /tmp/$Rev/lib/ || die "libBarcode.so"
-    cp -v $builddir/$variant/$Apk /tmp/$Rev/internal/Game.apk || die "$Apk"
+    cp -v $builddir/$prjname/libmtkhw.so /tmp/$VerF/lib/ || die "libmtkhw.so"
+    cp -v $builddir/$prjname/libs/armeabi-v7a/libBarcode.so /tmp/$VerF/lib/ || die "libBarcode.so"
+    cp -v $builddir/$variant/$Apk /tmp/$VerF/internal/Game.apk || die "$Apk"
 
-    find /tmp/$Rev -type f -exec chmod 0644 '{}' \;
-    ( cd /tmp && rsync -vr $Rev $rhost:$variant/application/ ) || die "rsync $rhost"
-    rm -rf /tmp/$Rev
+    find /tmp/$VerF -type f -exec chmod 0644 '{}' \;
+    ( cd /tmp && rsync -vr $VerF $rhost:$variant/application/ ) || die "rsync $rhost"
+    rm -rf /tmp/$VerF
     ;;
 
 prepare|init)
     svn st $repo
-    OldSVNRev=`tr -d ' \t' <$repo/$AppConfig |grep -Po '^publicstaticfinalStringSVNVERSION="new-svn\K[^"]+'`
 
-    if [ "$variant" = release ] ; then
-        if [ -z "$Newver" ] ; then
-            Newver=`echo $Ver | awk -F. '{print $1"."$2"."$3+1}'`
-            [ -n "$Newver" ] || die "Incr VERSION fail"
-        fi
-        sed -i '/^\s*public.\+\<VERSION\s*=/{s/"v[0-9]\+\.[0-9]\+\.[0-9]\+"/"v'$Newver'"/}' $repo/$AppConfig
-        sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' $repo/$AppConfig
-    fi
-
-    svname=`basename $repo`
-    rm -rf $builddir/$svname
-    ( cd $repo/.. && find $svname \( -name ".svn" -o -name ".git*" \) -prune -o -print \
+    rm -rf $builddir/$prjname
+    ( cd $repo/.. && find $prjname \( -name ".svn" -o -name ".git*" \) -prune -o -print \
         | rsync --files-from=- . $builddir )
 
     if [ "$variant" = release ] ; then
-        sed -i '/^[^#]\+#\s*define\s\+BUILD_RELEASE/{s:^[^#]\+::}' $builddir/$svname/jni/Utils/log.h
-        cp -v tools/CryptoRelease.bat $builddir/$svname/tools/Crypto.bat
+        cp -v tools/CryptoRelease.bat $builddir/$prjname/tools/Crypto.bat
+        sed -i '/^\s*public.\+\<VERSION\s*=/{s/"v[0-9]\+\.[0-9]\+\.[0-9]\+"/"v'$NewVer'"/}' $builddir/$prjname/$AppConfig
+        sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' $builddir/$prjname/$AppConfig
+        sed -i '/^[^#]\+#\s*define\s\+BUILD_RELEASE/{s:^[^#]\+::}' $builddir/$prjname/jni/Utils/log.h
     else
-        sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' \
-            $builddir/$svname/$AppConfig
-        testoutdir=" \\\\192.168.2.115\\Release1\\test\\"
+        sed -i '/^\s*public.\+\<SVNVERSION\s*=/{s/"new-svn[0-9]\+"/"new-svn'$NewSVNRev'"/}' $builddir/$prjname/$AppConfig
+        testoutdir=" \\\\192.168.2.115\\Release1\\test\\ ${Apk%.apk}"
     fi
 
-    echo ": diff -r --brief $repo $builddir/$svname"
-    diff -r --brief $repo $builddir/$svname |grep -v '.svn'
+    echo ": diff -r --brief $repo $builddir/$prjname"
+    diff -r --brief $repo $builddir/$prjname |grep -v '.svn'
     echo
-    diff $repo/$AppConfig $builddir/$svname/$AppConfig
-    diff $repo/jni/Utils/log.h $builddir/$svname/jni/Utils/log.h
-    #diff --brief $repo/../tools/CryptoRelease.bat $builddir/$svname/tools/Crypto.bat
+    diff $repo/$AppConfig $builddir/$prjname/$AppConfig
+    diff $repo/jni/Utils/log.h $builddir/$prjname/jni/Utils/log.h
+    #diff --brief $repo/../tools/CryptoRelease.bat $builddir/$prjname/tools/Crypto.bat
 
     #echo
     #find $builddir/$repo -name log.h -o -name AppConfig.java -o -name Crypto.bat
 
-    echo "SVN Revision: $OldSVNRev => $NewSVNRev"
-    echo "Version: $Ver => $Newver $Vertag"
     echo
-    svn log -r$OldSVNRev:$NewSVNRev $repo > svn.log.$OldSVNRev-$NewSVNRev
-    echo "svn.log.$OldSVNRev-$NewSVNRev"
+    echo "Version: $OldVer => $NewVer, $OldSVNRev => $NewSVNRev" |tee -a svn-log.$NewVer
+    svn log -r$OldSVNRev:$NewSVNRev $repo >> svn-log.$NewVer
+    echo "svn-log.$NewVer"
     echo "$variant/$Apk$testoutdir"
     # grep word howto.txt
-    echo
-    svn st $repo
-    #echo "svn diff $repo/$AppConfig"
-    #echo "svn commit androidbarcode"
+    #echo
     ;;
 
 esac
