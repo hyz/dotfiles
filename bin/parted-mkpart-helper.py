4c15744f40d427bf4d64f2986020327e1a411964
hyz <jiyong.wu@gmail.com>
2017-01-06 16:14:58 +0800

    u

--- bin/parted-mkpart-helper.py
+++ bin/parted-mkpart-helper.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
+
 import sys
 
-def main(sec_size, sec_begin, exp_size):
-    a = M/sec_size
+# Sector size (logical/physical): 512B/4096B
+sec_size_log = 512
+sec_size_phy = 4096
+
+def physical_align(a, sec_begin, n_sec):
     sec_begin = int((sec_begin + (a-1)) /a) * a    # aligned START
-    sec_end = sec_begin + exp_size/sec_size
+    sec_end = sec_begin + n_sec
     sec_end = int((sec_end + (a-1)) /a) * a    # aligned START
-    return sec_begin, sec_end-1
+    return int(sec_begin), int(sec_end)-1
 
 if __name__ == '__main__':
     try:
-        sec_size, sec_begin, exp_size = '4096B', sys.argv[1], sys.argv[2]
-        if len(sys.argv) > 3:
-            sec_size, sec_begin, exp_size = sys.argv[1], sys.argv[2], sys.argv[3]
-        assert sec_size[-1] == 'B'
+        assert sec_size_phy % sec_size_log == 0
+        assert len(sys.argv) == 3
+        _, sec_begin, exp_size = '512B/4096B', sys.argv[1], sys.argv[2]
+        #if len(sys.argv) > 3: sec_size_phy, sec_begin, exp_size = sys.argv[1], sys.argv[2], sys.argv[3]
+        #assert sec_size_phy[-1] == 'B'
         assert sec_begin[-1] == 's'
         assert exp_size[-1] in ('G','M')
-
         G, M = 1024*1024*1024, 1024*1024
+
+        #sec_size_phy = int(sec_size_phy.rstrip('B'))
+        sec_begin = max(int(sec_begin.rstrip('s')) * sec_size_log, sec_size_phy)
         if exp_size[-1] == 'G':
             exp_size = int(exp_size.rstrip('G'))*G
         else:
             exp_size = int(exp_size.rstrip('M'))*M
-
-        sec_begin = int(sec_begin.rstrip('s'))
-        sec_size = int(sec_size.rstrip('B'))
-
-        print('mkpart primary', *map(lambda x:x+'s', map(str,map(int,main(sec_size, sec_begin, exp_size)))) )
+        v = physical_align(M/sec_size_phy, sec_begin/sec_size_phy, exp_size/sec_size_phy)
+        v = map(lambda x:'%.0fs'%(x*sec_size_phy/sec_size_log), v)
+        print('mkpart primary', *v)
     except Exception:
         print('Example:')
-        print('\talign-parted.py 4096B 1260218368s 32G')
+        print('\t%s 4096B 1260218368s 32G' % sys.argv[0])
         print()
         raise
 
