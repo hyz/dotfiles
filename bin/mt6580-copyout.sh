8d46513dd566fccfdcd0fb9186d649d0706a0f84
hyz <jiyong.wu@gmail.com>
2017-02-24 18:56:08 +0800

    u

--- bin/mt6580-copyout.sh
+++ bin/mt6580-copyout.sh
@@ -3,7 +3,8 @@
 out="$1"
 outdb="$1/database"
 
-[ ! -d "out" ] || rm -rf "$out"
+[ ! -z "$out" ] || exit 1
+[ ! -d "$out" ] || rm -rf "$out"
 mkdir -p "$outdb" || exit 2
 
 BPLGUInf=BPLGUInfoCustomAppSrcP_MT6580*
@@ -12,7 +13,10 @@ lunch=out/target/product/ckt6580_we_l
 objCGEN=$lunch/obj/CGEN/APDB_MT6580*
 objETC=$lunch/obj/ETC/$BPLGUInf/$BPLGUInf
 
-echo    "copy $objCGEN to $outdb" ; cp -vt $outdb $objCGEN
-echo    "copy $objETC to $outdb" ; cp -vt $outdb $objETC
-echo "copy $lunch to $out" ; find $lunch -maxdepth 1 -type f | xargs cp -vt $out 
+#echo    "copy $objCGEN to $outdb"
+cp -vt $outdb $objCGEN
+#echo    "copy $objETC to $outdb"
+cp -vt $outdb $objETC
+#echo "copy $lunch to $out" 
+find $lunch -maxdepth 1 -type f | xargs cp -vt $out 
 
