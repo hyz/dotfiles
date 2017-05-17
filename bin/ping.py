923ab40ca0247c91830d012ab9bc30710e385d89
oops <cooloops@qq.com>
2017-05-17 15:08:05 +0800

    u

--- bin/ping.py
+++ bin/ping.py
@@ -4,8 +4,7 @@ import sys, re
 # [1494033662.691981] 64 bytes from 192.168.0.1: icmp_seq=235 ttl=64 time=41.3 ms
 
 tims = []
-tims_5 = []
-tims1 = []
+tims1, tims_5, tims_2 = [], [], []
 ignores = []
 
 for line in sys.stdin:
@@ -17,14 +16,17 @@ for line in sys.stdin:
         continue
     t = float(res.group(1))
     tims.append(t)
-    if t >= 500:
-        tims_5.append(t)
-    if t >= 1000:
-        tims1.append(t)
+    if t >= 200:
+        tims_2.append(t)
+        if t >= 500:
+            tims_5.append(t)
+            if t >= 1000:
+                tims1.append(t)
 
-print('average/max/min:',sum(tims) / len(tims), max(tims), min(tims))
-print('>=500 count:', len(tims_5), float(len(tims_5))*100/len(tims))
-print('>=1000 count:', len(tims1), float(len(tims1))*100/len(tims))
+print('average/max/min:', sum(tims) / len(tims), max(tims), min(tims))
+print('>=200' ,'count:', len(tims_2), float(len(tims_2))*100/len(tims), sep='\t')
+print('>=500' ,'count:', len(tims_5), float(len(tims_5))*100/len(tims), sep='\t')
+print('>=1000','count:', len(tims1) , float(len(tims1))*100/len(tims) , sep='\t')
 print('total count:', len(tims))
 
 for line in ignores:
--- notes/android/cross-iperf3.sh
+++ notes/android/cross-iperf3.sh
@@ -24,3 +24,16 @@ export PATH=$PATH:$TOOLC/bin
 #You can compile and link a PIE executable in one of two ways. First, compile everything with -fPIE and link with -pie. The second is to compile everything with -fPIC and link with -pie.
 #If you are building both a shared object and a program, then compile everything with -fPIC. Link the shared object with -shared, and link the program with -pie.
 
+### stackoverflow.com/questions/40975003/iperf3-execution-from-android-7-doesnt-work
+# Tip: iperf3 uses an folder to cache the results which is inaccessible for Android. You'll need to change this folder to work:
+# https://github.com/esnet/iperf/blob/670c18584bcf7a285f3561eb7ea38cc53600d0ab/src/iperf_api.c#L2621
+#. android 7.0, has no `/tmp' directory, use `/data/local/tmp' instead
+
+#
+# iperf3 -c 192.168.0.20 -u -M 1456 -b 100M
+#
+# adb -d shell /data/iperf3 -c 192.168.0.1 -u -M 1456 -b 100M                  
+#
+
+###1 vim src/Makefile --- remove flags: -pg -g
+
