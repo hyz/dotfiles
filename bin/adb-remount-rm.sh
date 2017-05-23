0399863c9535b56922bb5dc884d96d5ff94261d4
oops <cooloops@qq.com>
2017-05-23 09:55:56 +0800

    u

---
+++ bin/adb-remount-rm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+adb -d shell mount -o rw,remount,rw /system # adb -d remount
+adb -d shell "mount -t rootfs -o remount,rw rootfs /system"
+
+for x in Game.apk libBarcode.so libmtkhw.so ; do
+    adb -d shell rm -f /system/app/$x
+    adb -d shell rm -f /system/lib/$x
+    adb -d shell rm -f /system/app/Game/$x
+done
+
+adb -d shell reboot
+
